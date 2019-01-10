258307c09ab56bb009c8bb535fdc725d01bc13d7
RomainMaillot <romainmaillot.rm@gmail.com>
2019-01-10 10:55:57 +0100

    Starting server

--- 10-01/server.js
+++ 10-01/server.js
@@ -1,14 +1,26 @@
 /*
 Imports and configuration
 */
+    require('dotenv').config()
+
     const express = require('express')
+    const path = require('path')
+    const ejs = require('ejs')
 //
 
 /*
 Server config.
 */
     const server = express()
-    const port = 9845
+    const port = process.env.PORT
+
+    //=> Set views engine
+    server.engine('html', ejs.renderFile)
+    server.set('view engine', 'html')
+
+    //=> Use path to add views
+    server.set('views', __dirname + '/www')
+    server.use(express.static(path.join(__dirname, 'www')))
 //
 
 /*
